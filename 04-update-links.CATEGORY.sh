604d568d72188e271edf5c33b714f2355e66dfc4
Douglas J Bothwell <doug.bothwell.opnet@gmail.com>
2022-11-11 14:50:52 -0800

    update shell script per DOC-2232

--- 04-update-links.CATEGORY.sh
+++ 04-update-links.CATEGORY.sh
@@ -1,14 +1,16 @@
 #bin/bash
 
 echo "========================================"
-echo "Running ./STAGE_docs/get-images.DEV.py:"
+echo "Running ./STAGE_docs/update-links.CATEGORY.sh:"
 echo "========================================"
 ls ./IN_html
 
 cd ./STAGE_docs/
 
-rm ../_logs/update-links.CATEGORY.log
-echo "Starting python script: " 
+rm ./_logs/update-links.CATEGORY.log
+rm ./_data/category-mappings.LATEST.json
+
+echo "Starting python script: hd-convert/_logs/update-links.CATEGORY.log " 
 python3  update-links.CATEGORY.py > ../_logs/update-links.CATEGORY.log
 echo "python script ended, links updated."
 echo "========================================"
