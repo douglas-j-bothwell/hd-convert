88f363fee4b5d6d5dc80e1de9fa250700a0f616d
Douglas J Bothwell <doug.bothwell.opnet@gmail.com>
2022-11-11 14:46:19 -0800

    Bug fix for DOC-2232
    also added function to save categories catalog to _data/category-mappings.LATEST.json

--- STAGE_docs/update-links.CATEGORY.py
+++ STAGE_docs/update-links.CATEGORY.py
@@ -8,33 +8,16 @@ import yaml
 import json
 import pprint
 
-'''
-https://regex101.com/
-test strings:
-(https://ngdocs.harness.io/article/e7yidxmtmj-add-azure-connector#before_you_begin)
-(/article/e7yidxmtmj)
-(https://kubernetes.io/docs/tutorials/kubernetes-basics/update/update-intro/) or a [Canary Deployment](https://www.infoworld.com/article/3644449/how-canary-releases-enable-continuous-deployment.html)
-(https://app.harness.io/auth/#/signup/?module=cf).  Creating and executing a Feature Flag for the React app is pretty straightforward. Depending on your language, Feature Flags requires the use of a corresponding SDK (software development kit), which is installed as a dependency in your application.  Since this example application is React, we can leverage the [JavaScript Feature Flags SDK](https://docs.harness.io/article/bmlvsxhp13-java-script-sdk-references)
-Since this example application is React, we can leverage the [JavaScript Feature Flags SDK](https://docs.harness.io/article/bmlvsxhp13-java-script-sdk-references)
-Since this example application is React, we can leverage the [JavaScript Feature Flags SDK](/article/e7yidxmtmj)
-(/article/ltt65r6k39-set-up-cost-visibility-for-kubernetes), [AWS](/article/80vbt5jv0q-set-up-cost-visibility-for-aws), [GCP](/article/kxnsritjls-set-up-cost-visibility-for-gcp), and [Azure](/article/v682mz6qfd-set-up-cost-visibility-for-azure)
-'''
 
-'''
-(.*?\/article\/[^)]*?)\)
-(https://ngdocs.harness.io/article/e7yidxmtmj-add-azure-connector#before_you_begin)
-(/article/e7yidxmtmj)
-(https://kubernetes.io/docs/tutorials/kubernetes-basics/update/update-intro/) or a [Canary Deployment](https://www.infoworld.com/article/3644449/how-canary-releases-enable-continuous-deployment.html)
-'''
+
+# _topicCategoryURL_RE = re.compile('\(\/category\/.*?\.*?\)')
+# _topicLinkRE = re.compile('\((.*\/article\/[^)]*)\)')
+# _topicIDRE = re.compile('helpdocs_category_id:.*$')
 
 _topicLinkRE = re.compile('\((https:\/\/n?g?docs.harness.io\/article\/|/article\/).*?\)')
 _categoryLinkRE = re.compile('\((https:\/\/n?g?docs.harness.io\/category\/|/category\/).*?\)')
-# _topicCategoryURL_RE = re.compile('\(\/category\/.*?\.*?\)')
-# _topicLinkRE = re.compile('\((.*\/article\/[^)]*)\)')
-
-_topicIDRE = re.compile('helpdocs_category_id:.*$')
+_categoriesDataFile = '../_data/category-mappings.LATEST.json'
 _mdRoot = './docs/'
-_topicMap = {}
 _categoryMap = {}
 _linksNotUpdated = []
 
@@ -82,61 +65,30 @@ def stringListToFile(strList, fileName):
     with open(fileName, 'w') as f:
         f.writelines(strList)
 
-def updateLineWithDomain(mdFileName, line, idx, cDict):
+def updateLine(mdFileName, line, idx, cDict):
     newLine = line
     linkIsInDictionary = False
     linkNeedsDomain = True
     for match in re.finditer(_categoryLinkRE, line):
          curLink = match.group()
-         if curLink in cDict.values(): 
-             linkIsInDictionary = True
-             print("[DEBUG5] Skipping, link ", curLink, " is in dictionary with value ", cDict(curLink))
-         if "docs.harness.io" in curLink:
-             linkNeedsDomain = False
-             print("[DEBUG5] Skipping, link already has domain: ", curLink,)
-         if linkIsInDictionary == False and linkNeedsDomain == True:
-            print("\n[WARNING] The following link might need updating:")
-            print('\tFile: ', mdFileName)
-            print('\tline ', idx, 'original: ', line)
-            print("\t You need to add \'https://docs.harness.io\' to this link the category ID isn't in the following dictionary:")
-            print(cDict)
-            print('-----------------')
-            '''
-            curLinkPlusDomain = curLink.replace("(", "(https://docs.harness.io")     
-            newLine = newLine.replace(curLink, curLinkPlusDomain)
-            print("\n[UPDATE_LINK_SUCCESS1] Added domain to link:")
-            print('\tline ', idx, 'original: ', line)
-            print('\tline ', idx, 'updated : ', newLine)
-            ''' 
-         
-    return newLine        
-
-
-def updateLineWithLocalTarget(mdFileName, line, idx, cDict):
-    newLine = line
-    for match in re.finditer(_categoryLinkRE, line):
-         curLink = match.group()
-         newLinkLocalTarget = False
-         skipUpdate = False
          hd_ID = getHelpDocsID(curLink, "/category/", cDict)
-         print ("[DEBUG2] curLink = ", curLink)
-         print("[DEBUG2] line ", idx, '\t', line)
-
+         
+         # case 1: found category ID in catalog, replace current link with (/catagory/<developer-hub-category-string>)
          if hd_ID != False: 
             newLinkLocalTarget = cDict[hd_ID]
-            newLinkLocalTarget = '(' + newLinkLocalTarget + ')' 
-            print("[DEBUG3] Category ID found. curLink = ", curLink, " helpdocs ID = ", hd_ID, "\tnewLinkLocalTarget = ", newLinkLocalTarget)
-                     
-         # if we found a local category, replace curLink with newLinkLocalTarget.
-         # print("[DEBUG1] line ", idx, '\t', line)
-         if newLinkLocalTarget != False:
-            # curLink = curLink.replace("(https://docs.harness.io", "")
-            newLine = newLine.replace(curLink, newLinkLocalTarget)
-            print("[DEBUG3] After replace. curLink = ", curLink, "\tnewLinkLocalTarget = ", newLinkLocalTarget)
-            print("\n[UPDATE_LINK_SUCCESS2] Replaced with local target:", idx)
-            print('\tline ', idx, 'original: ', line)
-            print('\tline ', idx, 'updated : ', newLine) 
-    return newLine
+            newLinkLocalTarget = '(' + newLinkLocalTarget + ')'
+            newLine = newLine.replace(curLink, newLinkLocalTarget) 
+            # print("[DEBUG7] Category ID found. curLink = ", curLink, " helpdocs ID = ", hd_ID, "\tnewLinkLocalTarget = ", newLinkLocalTarget)
+            continue
+         else:
+            # case 1: category has no local equivalent and no linkNeedsDomain, add domain to current link so it points to the HD target 
+            if ("docs.harness.io" in curLink) == False: 
+                curLinkPlusDomain = curLink.replace("(", "(https://docs.harness.io")     
+                newLine = newLine.replace(curLink, curLinkPlusDomain)
+                # print("[DEBUG7] Adding domain. curLink = ", curLink, "\tcurLinkPlusDomain = ", curLinkPlusDomain)
+                continue
+    return newLine        
+
 
 def updateTopic(mdFileName, cDict):
     updated = False
@@ -147,12 +99,12 @@ def updateTopic(mdFileName, cDict):
         newMarkdown = []
         topicUpdated = False 
         for line in mdLines:
-            newLine = updateLineWithDomain(mdFileName, line, idx, cDict)
-            newLine = updateLineWithLocalTarget(mdFileName, newLine, idx, cDict)
-            
+            newLine = updateLine(mdFileName, line, idx, cDict)
             newMarkdown.append(newLine)
             if (line != newLine):
-                print("[DEBUG4] newLine updated: ", newLine)
+                print("\n[UPDATE_LINK_SUCCESS7] link updated in ", mdFileName)
+                print('\tline ', idx, 'original: ', line)
+                print('\tline ', idx, 'updated : ', newLine)     
                 topicUpdated = True
             idx += 1
         stringListToFile(newMarkdown, mdFileName)
@@ -179,6 +131,6 @@ for mdFileName in glob.iglob(_mdRoot + '**/**', recursive=True):
 pp1 = pprint.PrettyPrinter(depth=4)
 pp1.pprint(_categoryMap)
 
-"\n\\n[DEBUG] links not updated ="
-pp2 = pprint.PrettyPrinter(depth=4)
-pp2.pprint(_linksNotUpdated)
+
+with open(_categoriesDataFile, 'w') as fp:
+    json.dump(_categoryMap, fp)
