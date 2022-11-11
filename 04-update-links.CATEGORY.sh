89cdc3b1daec8eaad156f5fa3ca22646daa11531
Douglas J Bothwell <doug.bothwell.opnet@gmail.com>
2022-11-11 14:52:45 -0800

    fixed output in shell script

--- 04-update-links.CATEGORY.sh
+++ 04-update-links.CATEGORY.sh
@@ -10,7 +10,7 @@ cd ./STAGE_docs/
 rm ./_logs/update-links.CATEGORY.log
 rm ./_data/category-mappings.LATEST.json
 
-echo "Starting python script: hd-convert/_logs/update-links.CATEGORY.log " 
+echo "Starting python script: hd-convert/STAGE_docs/update-links.CATEGORY.py " 
 python3  update-links.CATEGORY.py > ../_logs/update-links.CATEGORY.log
 echo "python script ended, links updated."
 echo "========================================"
