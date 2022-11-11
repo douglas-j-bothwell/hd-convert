38560cadc27dddf55052f437a20b6debaec791fe
Douglas J Bothwell <doug.bothwell.opnet@gmail.com>
2022-11-10 16:18:53 -0800

    Update 00_init.sh
    comment out node-install commands (redundant) and add command to install Requests python library

--- 00_init.sh
+++ 00_init.sh
@@ -1,27 +1,28 @@
 
 # install homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Run these three commands in your terminal to add Homebrew to your PATH:
-echo '# Set PATH, MANPATH, etc., for Homebrew.' >> /Users/admin/.zprofile
-echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/admin/.zprofile
-eval "$(/opt/homebrew/bin/brew shellenv)"
+# echo '# Set PATH, MANPATH, etc., for Homebrew.' >> /Users/admin/.zprofile
+# echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/admin/.zprofile
+# eval "$(/opt/homebrew/bin/brew shellenv)"
 
-brew install node
+# brew install node
 
 brew install python3
 pip3 install markdownify
 pip3 install pyyaml
+pip3 install requests
 
 
 #Install Latest Node
-brew install node
+#brew install node
 
 #If a Specific Version of Node is Required
-node --version
-brew search node
-brew unlink node
-brew install node@16
-brew link node@16
-brew link --overwrite node@16
-brew switch
+# node --version
+# brew search node
+# brew unlink node
+# brew install node@16
+# brew link node@16
+# brew link --overwrite node@16
+# brew switch
